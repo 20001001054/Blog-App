afb22e84065bd0a64ee3d3f186bd1fa9d332245b
KABIR <20001001054kabir@dcrustm.org>
2024-04-21 21:29:48 +0530

    .

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,40 @@
-
+import React from 'react'
+import { useState,useEffect } from 'react'
+import {useDispatch} from 'react-redux'
 import './App.css'
+import  authservice from './Appwrite/auth'
+import {login,logout} from './Store/authSlice'
+import { Header, Footer } from './components'
+import { Outlet } from 'react-router-dom'
 
 function App() {
-  console.log(import.meta.env.VITE_APPWRITE_URL)
+  const [loading, setLoading ] = useState(true)
+  const dispatch = useDispatch()
 
-  return (
-    <>
-     <h1>Blog app</h1>
-    </>
-  )
+  useEffect(() =>{
+    authservice.getCurrentUser()
+    .then((userData) =>{
+      if(userData){
+        dispatch(login({userData}))
+      }else{
+        dispatch(logout())
+      }
+    })
+    .finally(() => setLoading(false))
+  },[])
+
+ 
+  return !loading ?(
+    <div className='min-h-screen flex flex-wrap content-between bg-gray-400'>
+      <div className='w-full block'>
+        <Header />
+        <main>
+        TODO:   <Outlet /> 
+        </main>
+        <Footer />
+      </div>
+    </div>
+  ) : null
 }
 
 export default App
