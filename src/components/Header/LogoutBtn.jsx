9aa389dc0346f4e270f2127a4fd4a74f8d4c8aa2
KABIR <20001001054kabir@dcrustm.org>
2024-04-22 12:18:03 +0530

    .

---
+++ src/components/Header/LogoutBtn.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import {useDispatch} from 'react-redux'
+import authService from '../../Appwrite/config'
+import {logout} from '../../Store/authSlice'
+
+function LogoutBtn() {
+    const dispatch = useDispatch();
+    const logoutHandler = () =>{
+        authService.logout().then(() => {
+                dispatch(logout())
+        })
+    }
+  return (
+    <div>
+      <button className='inlinee-block px-6 py-2 duration-200 hover:bg-blue-100 rounded-full'>Logout</button>
+    </div>
+  )
+}
+
+export default LogoutBtn
